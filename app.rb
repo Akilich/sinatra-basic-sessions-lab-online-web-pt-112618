ea6b96404711651f61dc0ae039d07b1709c55c2a
Akili Humphrey <akilihumphrey@gmail.com>
2019-03-19 19:30:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+    
+    post '/checkout' do
+      session_secret[:item] = params[:item]
+    end
 end
